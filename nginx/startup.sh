271c87e64128c408382327b19c11e67a231ba669
Robert Norman <rnorman@tripadvisor.com>
2019-03-18 18:17:21 +0000

    Adding ssl certificate for .test domains

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -6,4 +6,11 @@ if [ ! -f /etc/nginx/ssl/default.crt ]; then
     openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
 fi
 
+if [ ! -f /etc/nginx/ssl/hwd.crt ]; then
+    openssl req -x509 -out hwd.crt -keyout hwd.key \
+      -newkey rsa:2048 -nodes -sha256 \
+      -subj '/CN=www.holidaywatchdog.test' -extensions EXT -config <( \
+       printf "[dn]\nCN=www.holidaywatchdog.test\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:www.holidaywatchdog.test\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+fi
+
 nginx
